e4bf122a35a9e71b1f78803109a72131f474bb77
pan3ng <mphopan@gmail.com>
2019-11-05 06:56:13 +0200

    First flask project

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'This is a Flaak application.'
+
+#app.run('0.0.0.0:8080', debug=True)
+
+if __name__ == '__main__':
+    app.run()
